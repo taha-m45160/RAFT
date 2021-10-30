f6543fe0ff2a86f48e969e1ca9c0de84bb9e4769
Muhammad Taha <23100107@lums.edu.pk>
2021-10-30 15:53:26 +0500

    cleaner version

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -84,7 +84,6 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 }
 
 func (rf *Raft) Kill() {
-	// Your code here, if desired.
 }
 
 func Make(peers []*labrpc.ClientEnd, me int, persister *Persister, applyCh chan ApplyMsg) *Raft {
@@ -109,60 +108,7 @@ func Make(peers []*labrpc.ClientEnd, me int, persister *Persister, applyCh chan 
 	return rf
 }
 
-/*sends empty AppendEntries RPC as heartbeat message*/
-func heartBeat(rf *Raft) {
-	// get state
-	rf.mu.Lock()
-	totalPeers := len(rf.peers)
-	term := rf.currentTerm
-	rf.mu.Unlock()
-
-	storeReplies := make(map[int]*AppendEntriesReply)
-	Args := AppendEntriesArgs{term, rf.me}
-
-	// send heartbeats
-	for i := 0; i < totalPeers; i++ {
-		if i != rf.me {
-			storeReplies[i] = new(AppendEntriesReply)
-			go rf.sendAppendEntries(i, Args, storeReplies[i])
-		}
-	}
-}
-
-func nodeMain(rf *Raft, me int) {
-	rand.Seed(time.Now().UnixNano())
-	min := 350
-	max := 600
-
-	for {
-		rf.mu.Lock()
-		state := rf.state
-		rf.mu.Unlock()
-
-		switch state {
-		case "follower":
-			select {
-			case <-time.After(time.Duration(rand.Intn(max-min)+min) * time.Millisecond):
-				// leader timed out
-				rf.mu.Lock()
-				rf.state = "candidate"
-				rf.mu.Unlock()
-			case <-rf.heartBeat:
-				// timeout reset
-			case <-rf.voteRequested:
-				// timeout reset
-			}
-		case "candidate":
-			election(rf)
-		case "leader":
-			heartBeat(rf)
-			t := make(chan bool)
-			h := true
-			timer(t, h)
-		}
-	}
-}
-
+/*election process conducted upon leader failure*/
 func election(rf *Raft) {
 	rf.mu.Lock()
 	rf.currentTerm += 1
@@ -214,6 +160,7 @@ func election(rf *Raft) {
 	rf.mu.Unlock()
 }
 
+/*request vote rpc received and reply dispatched*/
 func (rf *Raft) RequestVote(args RequestVoteArgs, reply *RequestVoteReply) {
 	rf.voteRequested <- -1
 
@@ -240,6 +187,7 @@ func (rf *Raft) RequestVote(args RequestVoteArgs, reply *RequestVoteReply) {
 	}
 }
 
+/*append entries rpc received and reply dispatched*/
 func (rf *Raft) AppendEntries(args AppendEntriesArgs, reply *AppendEntriesReply) {
 	rf.mu.Lock()
 	currentTerm := rf.currentTerm
@@ -259,6 +207,7 @@ func (rf *Raft) AppendEntries(args AppendEntriesArgs, reply *AppendEntriesReply)
 	}
 }
 
+/*request vote rpc sent and reply handled*/
 func (rf *Raft) sendRequestVote(server int, args RequestVoteArgs, reply *RequestVoteReply) bool {
 	ok := rf.peers[server].Call("Raft.RequestVote", args, reply)
 
@@ -281,6 +230,7 @@ func (rf *Raft) sendRequestVote(server int, args RequestVoteArgs, reply *Request
 	return ok
 }
 
+/*append entries rpc sent and reply handled*/
 func (rf *Raft) sendAppendEntries(server int, args AppendEntriesArgs, reply *AppendEntriesReply) bool {
 	ok := rf.peers[server].Call("Raft.AppendEntries", args, reply)
 
@@ -302,7 +252,7 @@ func (rf *Raft) sendAppendEntries(server int, args AppendEntriesArgs, reply *App
 	return ok
 }
 
-/*return -1 when the timer runs out*/
+/*return false when the timer runs out*/
 func timer(timeout chan bool, heartBeat bool) bool {
 	if !heartBeat {
 		rand.Seed(time.Now().UnixNano())
@@ -320,5 +270,60 @@ func timer(timeout chan bool, heartBeat bool) bool {
 		time.Sleep(40 * time.Millisecond)
 
 		return false
+	}
+}
+
+/*sends empty AppendEntries RPC as heartbeat message*/
+func heartBeat(rf *Raft) {
+	// get state
+	rf.mu.Lock()
+	totalPeers := len(rf.peers)
+	term := rf.currentTerm
+	rf.mu.Unlock()
+
+	storeReplies := make(map[int]*AppendEntriesReply)
+	Args := AppendEntriesArgs{term, rf.me}
+
+	// send heartbeats
+	for i := 0; i < totalPeers; i++ {
+		if i != rf.me {
+			storeReplies[i] = new(AppendEntriesReply)
+			go rf.sendAppendEntries(i, Args, storeReplies[i])
+		}
+	}
+}
+
+/*RAFT peer main process*/
+func nodeMain(rf *Raft, me int) {
+	rand.Seed(time.Now().UnixNano())
+	min := 350
+	max := 600
+
+	for {
+		rf.mu.Lock()
+		state := rf.state
+		rf.mu.Unlock()
+
+		switch state {
+		case "follower":
+			select {
+			case <-time.After(time.Duration(rand.Intn(max-min)+min) * time.Millisecond):
+				// leader timed out
+				rf.mu.Lock()
+				rf.state = "candidate"
+				rf.mu.Unlock()
+			case <-rf.heartBeat:
+				// timeout reset
+			case <-rf.voteRequested:
+				// timeout reset
+			}
+		case "candidate":
+			election(rf)
+		case "leader":
+			heartBeat(rf)
+			t := make(chan bool)
+			h := true
+			timer(t, h)
+		}
 	}
 }
